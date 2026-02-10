42aeb4306cfe4f1798ade2389bd339a01150fe4c
Sudheer <sudheersudheer7654321@gmail.com>
2026-02-10 11:20:08 +0530

    feat: add RewardToken.sol

---
+++ contracts/RewardToken.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract RewardToken is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+}
