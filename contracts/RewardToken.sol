b97ddc3a3c383551eb8bb5619a086cbb502da810
Sudheer <sudheersudheer7654321@gmail.com>
2026-02-14 13:00:11 +0530

    feat: add intial 1 million tokens mint to msg.sender in Reward and Staking token contracts

--- contracts/RewardToken.sol
+++ contracts/RewardToken.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract RewardToken is ERC20 {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        // Mint 1 Million tokens to the deployer
+        _mint(msg.sender, 1000000 * 10**decimals());
+    }
 }
