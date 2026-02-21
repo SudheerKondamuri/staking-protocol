6faef6f957d957ca136d37d7f1a7641c85781b0b
Sudheer <sudheersudheer7654321@gmail.com>
2026-02-21 16:07:14 +0530

    chore(config): update hardhat network definitions and compiler settings

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,21 +1,36 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "hardhat-deploy"; // This is required for deploy scripts
-
+import "hardhat-deploy"; 
+import "hardhat-deploy-ethers";
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
-  namedAccounts: {
-    deployer: {
-      default: 0,
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      mining: {
+        auto: true,
+        interval: 0
+      }
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
     },
   },
   paths: {
-    deploy: 'deploy',
-    deployments: 'deployments',
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
   },
-  external: {
-    deployments: {}
-  }
 };
 
 export default config;
